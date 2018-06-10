7678816b9ca5fdebfb67bd0cacecd1dfbafdd6e8
drsenri <shibata.h.ac@gmail.com>
2018-06-11 00:40:56 +0900

    start: analysis on human protein network

--- rsrc/main.r
+++ rsrc/main.r
@@ -12,34 +12,57 @@ require(tidyverse)
 require(data.table)
 require(doParallel)
 require(graphkernels)
+require(cluster)
 require(maptools)
+source("rsrc/function.r", encoding = "utf-8")
 
 # read_data ---------------------------------------------------------------
 # all files were downloaded from http://hint.yulab.org/download/ at 2018/05/30
 
-d <- dir("data", full.names = T) %>% lapply(fread, data.table = F)
-names(d) <- dir("data") %>% str_remove("_binary_hq.txt$") # names = Species
+clust_species <- function() {
+  
+  d <- dir("data", full.names = T) %>% lapply(fread, data.table = F)
+  names(d) <- dir("data") %>% str_remove("_binary_hq.txt$") # names = Species
+  
+  # convert to graph --------------------------------------------------------
+  
+  d_graph <- d %>% map(convert_graph, left = "Gene_A", right = "Gene_B")
+  
+  # WL kernel ---------------------------------------------------------------
+  
+  WLmat <- CalculateWLKernel(d_graph, 5) %>% as.data.frame()
+  names(WLmat) <- names(d_graph)
+  
+  # k-means ---------------------------------------------------------------------
+  
+  # decide k (based on gap statistic)
+  gap <- clusGap(WLmat, kmeans, K.max = 10, B = 100, verbose = interactive())
+  gap_score <- gap$Tab[,"gap"]
+  gap_se <- gap$Tab[,"SE.sim"]
+  k <- min(which(gap_score - lag(gap_score, 1)  + lag(gap_se, 1) > 0)) # gap(k) >= gap(k+1) - SE.sim(k+1)となる最小のkを探す
 
-# convert to graph --------------------------------------------------------
+  # clustering
+  res <- kmeans(WLmat, k)
+  cluster <- res$cluster
+  names(cluster) <- names(d_graph)
+  
+  # plot --------------------------------------------------------------------
+  
+  # manually plot
+  pca <- WLmat %>% prcomp(scale = T)
+  if (!dir.exists("output"))
+    dir.create("output")
+  png("output/180604_species_cluster.png")
+  plot(x = pca$x[,1], y = pca$x[,2], col = cluster, cex = 1, pch = 15)
+  pointLabel(x = pca$x[,1], y = pca$x[,2], label = names(cluster), col = cluster)
+  dev.off()
+}
 
-d_graph <- d %>% map(convert_graph, left = "Gene_A", right = "Gene_B")
 
-# WL kernel ---------------------------------------------------------------
+# analysis on human protein network ---------------------------------------------------
 
-WLmat <- CalculateWLKernel(d_graph, 5) %>% as.data.frame()
-names(WLmat) <- names(d_graph)
+g <- fread("data/HomoSapiens_binary_hq.txt", data.table = F) %>% convert_graph(left = "Gene_A", right = "Gene_B")
+proteins <- vertex_attr(g)$name
+gl <- foreach(i = seq(proteins)) %do% induced_subgraph(g, proteins[i])
+names(gl) <- proteins
 
-# k-means ---------------------------------------------------------------------
-
-res <- kmeans(WLmat, 3)
-cluster <- res$cluster
-names(cluster) <- names(d_graph)
-
-# plot --------------------------------------------------------------------
-
-pca <- WLmat %>% prcomp(scale = T)
-dir.create("output")
-png("output/180604_species_cluster.png")
-plot(x = pca$x[,1], y = pca$x[,2], col = cluster)
-pointLabel(x = pca$x[,1], y = pca$x[,2], label = names(cluster))
-dev.off()
